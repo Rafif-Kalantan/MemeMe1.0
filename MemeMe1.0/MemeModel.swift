aca8cb50411a27809cc93573e24731dbf7f2fb21
Rafif Kalantan <11415238+Rafif-Kalantan@users.noreply.github.com>
2020-09-10 02:44:34 +0300

    Files

---
+++ MemeMe1.0/MemeModel.swift
@@ -0,0 +1,17 @@
+//
+//  MemeModel.swift
+//  MemeMe1.0
+//
+//  Created by Rafif Kalantan on 06/06/2020.
+//  Copyright © 2020 Rafif Kalantan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: NSString!
+    var bottomText: NSString!
+    var image: UIImage!
+    var memedImage: UIImage!
+}
